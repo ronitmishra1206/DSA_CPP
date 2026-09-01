39da153cb2ece50df32bfad9fc7daa00d73164db
ronitmishra1206 <ronitmishra1206@gmail.com>
2026-09-01 13:20:41 +0530

    Add DFS topological sort

---
+++ Graphs/topological_sort.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    private: 
+    void dfs(int node, vector<int>& vis, stack<int>& st, 
+             vector<vector<int>>& adj) {
+        
+        vis[node] = 1;
+        
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                dfs(it, vis, st, adj);
+            }
+        }
+        
+        st.push(node);
+    }
+
+public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        
+        vector<vector<int>> adj(V);
+        
+        for(auto edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+        
+        vector<int> vis(V, 0);
+        stack<int> st;
+        
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                dfs(i, vis, st, adj);
+            }
+        }
+        
+        vector<int> ans;
+        
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
