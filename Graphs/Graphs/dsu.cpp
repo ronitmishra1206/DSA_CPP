872446274020fb9ee23f60ed99a33188f2be9f7b
Ronit Mishra <ronitmishra1206@gmail.com>
2026-09-08 19:30:46 +0530

    Optimize DSU with path compression and union by rank

--- Graphs/Graphs/dsu.cpp
+++ Graphs/Graphs/dsu.cpp
@@ -1,22 +1,28 @@
-class DSU
-// basic DSU , optimization follows
-{
+int findd(int i, vector<int>& parent){ 
+    if(i == parent[i]){
+        return i;
+    }
 
-public:
-   int find(int i , vector<int>&parent){
-    if(i==parent[i]){
-    return i;
-   }
-   return find(parent[i],parent);
+    return parent[i] = findd(parent[i], parent); // path compression
 }
-void unionn(int x , int y , vector<int>&parent){
-    int x_parent = find(x,parent);
-    int y_parent = find(y,parent);
-    if(x_parent != y_parent){
+
+void uunion(int x, int y, vector<int>& parent, vector<int>& rank){ // union by rank
+
+    int x_parent = findd(x, parent);
+    int y_parent = findd(y, parent);
+
+    if(x_parent == y_parent){
+        return;
+    }
+
+    if(rank[x_parent] > rank[y_parent]){
+        parent[y_parent] = x_parent;
+    }
+    else if(rank[y_parent] > rank[x_parent]){
         parent[x_parent] = y_parent;
     }
+    else{
+        parent[x_parent] = y_parent;
+        rank[y_parent]++;
+    }
 }
-};
-
-
-
